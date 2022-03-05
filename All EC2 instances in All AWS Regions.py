525c0f1d7652966bb2930afe2b1012fc1326b89b
Shubham Mishra <shubhammishra892@gmail.com>
2022-03-05 16:37:15 +0800

    Update All EC2 instances in All AWS Regions.py

--- All EC2 instances in All AWS Regions.py
+++ All EC2 instances in All AWS Regions.py
@@ -2,7 +2,7 @@ import boto3
 #import pprint
 #creating AWS sessions
 
-session = boto3.Session(profile_name = "shubh", region_name = 'ap-southeast-1')
+session = boto3.Session(profile_name = "YOURPROFILENAME", region_name = 'YOURREGIONNAME')
 client = session.client(service_name = 'ec2')
 all_regions = client.describe_regions()
 
@@ -16,7 +16,7 @@ for each_reg in all_regions['Regions']:
 #print (list_of_aws_regions)
 
 for each_region in list_of_aws_regions:
-    session = boto3.Session(profile_name = "shubh", region_name = each_region)
+    session = boto3.Session(profile_name = "YOURPROFILENAME", region_name = each_region)
     resource = session.resource(service_name = "ec2")
     print ("List of EC2 Instances from the region:" ,each_region)
     for each_instance in resource.instances.all():
